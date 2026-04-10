c0b76bbde4bd3b34a3f96822fd06e4a542665640
apzkfhd <yagurtlove0829@gmail.com>
2026-04-10 11:12:57 +0900

    깃허브 배포 사이트 수정

--- js/script.js
+++ js/script.js
@@ -69,7 +69,7 @@ const publisherList = [
   },
   {
     numImg: "assets/images/publisher-4.png",
-    link: "https://apzkfhd.github.io/korea-gsuck-clone/g",
+    link: "https://apzkfhd.github.io/korea-gsuck-clone/",
     img: "assets/images/publisher-project-4.png",
     top: "클론코딩ㆍ리디자인",
     title: "대한민국 구석구석",
